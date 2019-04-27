e7e6557f70124af0c076529ff1bace2cb337cd22
Brad Cas <bmcasalvieri@gmail.com>
2019-04-27 19:39:18 -0400

    need to fix update portion

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,22 +24,25 @@ function start() {
   connection.query(queryString, function(err, products) {
     if (err) throw err;
     console.log(`\nITEMS FOR SALE`)
-    products.forEach(product => console.log(`Name: ${product.product_name} || ID: ${product.item_id} || Price: $${product.price}`));
+    products.forEach(product => console.log(`${product.product_name} || ID: ${product.item_id} || Price: $${product.price}`));
     console.log(`\n`);
-    
+
     // Create inquirer prompt
       // Ask user which product they would like to buy
       // Ask user how many units they would like to buy
       // Confirm order
-    const allProducts = products.map(product => product.product_name);
-    
     inquirer
       .prompt([
         {
-          type: "list",
-          message: "Which product would you like to buy?",
-          choices: allProducts,
-          name: "choice"
+          type: "input",
+          message: "Which product would you like to buy? Please enter ID number",
+          name: "choice",
+          validate: function(value) {
+            if (!isNaN(value)) {
+              return true;
+            };
+            return false;
+          }
         },
         {
           type: "input",
@@ -59,17 +62,40 @@ function start() {
           default: true
         }
       ]).then(function(res) {
-        console.log(res)
-      })
-  })
+        // If user doesn't confirm, start over
+        if (!res.confirm) {
+          start();
+        };
 
+        // Use array .find() method to find product user wants to purchase
+        const chosenProduct = products.find(product => product.item_id === parseInt(res.choice))
 
-}
-  // Select stock_quantity from database to ensure enough in stock to complete purchase
-    // If not
-      // Console.log "Insufficient Quantity"
-      // Run start() function
-    // If so
-      // Update store_quantity for the item
-      // Console.log total cost of purchase
+        // if amount desired < store_quantity, run an UPDATE query to update the stock_quantity value for that product
+        if (chosenProduct.stock_quantity >= parseInt(res.amount)) {
+          const queryString = "UPDATE products SET ? WHERE?";
+          connection.query(queryString,
+            [
+              {
+                stock_quantity: (chosenProduct.stock_quantity - parseInt(res.amount))
+              },
+              {
+                product_name: chosenProduct.product_name
+              }
+            ],
+            function(err) {
+              if (err) throw err;
+              const total = chosenProduct.price * res.amount;
+              console.log(`Your total is: $${total}`)
+              start();
+            }
+          );
+        }
+
+        else {
+          console.log(`Insufficient Quantity!`);
+          start();
+        };
+      });
+  });
+};
       
