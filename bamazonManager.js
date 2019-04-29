d04ecf93eb041d8f17001916d86642007bae2e76
Brad Cas <bmcasalvieri@gmail.com>
2019-04-29 13:58:05 -0400

    viewLowInventory() working for bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,31 +11,29 @@ const connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   start();
 })
 
 function start() {
   // Use inquirer to list menu options
-    // View roducts for sale
-    // View low inventory
-    // Add to inventory
-    // Add new product
+  // View roducts for sale
+  // View low inventory
+  // Add to inventory
+  // Add new product
   inquirer
-    .prompt([
-      {
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View Products for Sale",
-          "View Low Inventory",
-          "Add to Inventory",
-          "Add New Product"
-        ],
-        name: "choice"
-      }
-    ]).then(function(res) {
+    .prompt([{
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ],
+      name: "choice"
+    }]).then(function (res) {
       // Create switch statement for res.choice to run corresponding functions
       switch (res.choice) {
         case "View Products for Sale":
@@ -56,11 +54,28 @@ function start() {
 
 function viewProducts() {
   const queryString = "SELECT * FROM products"
-  connection.query(queryString, function(err, products) {
+  connection.query(queryString, function (err, products) {
     if (err) throw err;
-    console.log(`\nITEMS FOR SALE`)
+    console.log(`\nITEMS FOR SALE`);
     products.forEach(product => console.log(`${product.product_name} || ID: ${product.item_id} || Price: $${product.price} || Quantity: ${product.stock_quantity}`));
     console.log(`\n`);
+    start();
+  });
+};
+
+function viewLowInventory() {
+  const queryString = "SELECT * FROM products"
+  connection.query(queryString, function (err, products) {
+      if (err) throw err;
+
+      products.forEach(product => {
+        if (product.stock_quantity < 5) {
+          console.log(`\nLOW INVENTORY`);
+          console.log(`${product.product_name} || ID: ${product.item_id} || Price: $${product.price} || Quantity: ${product.stock_quantity}`);
+          console.log(`\n`);
+        };
+      });
+
     start();
   });
 };
